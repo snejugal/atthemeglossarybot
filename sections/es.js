3a2eac75918ca3b823f42e3b533eb66812b4dc90
DestroyerIV <32944302+DestroyerIV@users.noreply.github.com>
2017-11-06 04:35:16 +0100

    Added all sections of the Spanish glossary

--- sections/es.js
+++ sections/es.js
@@ -12,11 +12,47 @@ module.exports = [
     url: `http://telegra.ph/Variable-Textos-10-20`
   },
   {
+    title: `Controles`,
+    url: `http://telegra.ph/Variable-Controles-10-21`
+  },
+  {
     title: `Listas`,
     url: `http://telegra.ph/Variable-Listas-10-20`
   },
   {
     title: `Lista de chats`,
     url: `http://telegra.ph/Variable-Lista-de-chats-10-20`
+  },
+  {
+    title: `Llamadas`,
+    url: `http://telegra.ph/Variables-Llamadas-10-30`
+  },
+  {
+    title: `Barra de acciones`,
+    url: `http://telegra.ph/Variables-Barra-de-acciones-10-30`
+  },
+  {
+    title: `Diálogos`,
+    url: `http://telegra.ph/Variables-Di%C3%A1logos-10-30`
+  },
+  {
+    title: `Pantalla de chat`,
+    url: `http://telegra.ph/Variable-Pantalla-de-chat-10-22`
+  },
+  {
+    title: `Contactos`,
+    url: `http://telegra.ph/Variable-Contactos-10-22`
+  },
+  {
+    title: `Adjuntos`,
+    url: `http://telegra.ph/Variables-Adjuntos-10-21`
+  },
+  {
+    title: `Reproductor de música in-app`,
+    url: `http://telegra.ph/Variables-Reproductor-de-m%C3%BAsica-In-app-10-21`
+  },
+  {
+    title: `Ajustes`,
+    url: `http://telegra.ph/Variables-Ajustes-10-21`
   },
 ]
